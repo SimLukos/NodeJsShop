9617149249e43417086bce11e972fbe0c686cf7a
Simonas Lukoševičius <simonaslukosevicius@Simonass-MacBook-Pro.local>
2023-07-13 18:13:52 +0300

    first commit

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (callBack) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callBack([]);
+    } else {
+      callBack(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callBack) {
+    getProductsFromFile(callBack);
+  }
+};
